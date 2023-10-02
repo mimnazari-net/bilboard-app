45eb4a3dcac81bad4594a8ef45142ff4588db3fb
saghi <saghi7699@gmail.com>
2023-10-02 16:43:04 +0330

    create sellers

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,13 @@ import Mojtama from "./header/mojtama";
 import "./globals.css";
 import Newest from "./header/newset";
 import Dastebandi from "./header/dastebandi";
+import Forush from "./header/furush";
+import HouseCrad from "@/components/houseCard";
 
 export default function Home() {
   return (
-    <main><Header />
-    <Moshaverin/>  </main>
+    <main>
+    <Forush/> 
+    </main>
   );
 }
