22ef141364cb5c3f5dd404f64fc8cd3c2046eb0f
mimnazari-net <maryamnazari5258@gmail.com>
2023-11-14 14:55:41 +0330

    edit footer size

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -20,47 +20,54 @@ export default function Footer() {
           برای فروش ملک خود، به مشکل خورده اید؟
         </p>
         <p className="footer_image_text_desc">
-          با تیم مشاوران بیلبورد، به جواب پرسش های خود می رسید.{" "}
+          با تیم مشاوران بیلبورد، به جواب پرسش های خود می رسید.
         </p>
         <button className="footer_image_button">درخواست مشاوره</button>
       </div>
       {/* center section */}
       <div className="footer_texts_container ">
         <div className="footer_texts">
-          
-            <div className="footer_texts_city_col">
-              <p>شهرهای محبوب</p>
-              <div>تهران </div>
-              <div>کیش </div>
-              <div>مازندران </div>
-              <div>مشهد </div>
+          <div className="footer_texts_city_col">
+            <p className="footer_text_title">شهرهای محبوب</p>
+            <div className="footer_text_dec">
+              <p>تهران</p>
+              <p>کیش</p>
+              <p>مازندران</p>
+              <p>مشهد</p>
             </div>
+          </div>
 
-            <div className="footer_texts_disrtict_col">
-              <p>محله های محبوب</p>
-              <div>نیاوران </div>
-              <div>پاسداران </div>
-              <div>نارمک </div>
-              <div>فرمانیه </div>
+          <div className="footer_texts_disrtict_col">
+            <p className="footer_text_title">محله های محبوب</p>
+            <div className="footer_text_dec">
+              <p>نیاوران </p>
+              <p>پاسداران </p>
+              <p>نارمک </p>
+              <p>فرمانیه </p>
             </div>
+          </div>
 
-            <div className="footer_texts_search_col">
-              <p>بیشترین جستجو </p>
-              <div>خرید زمین </div>
-              <div>خرید ویلا زیباکنار</div>
-              <div>ویلا دوبلکس </div>
-              <div>آپارتمان ارزان </div>
+          <div className="footer_texts_search_col">
+            <p className="footer_text_title">بیشترین جستجو </p>
+            <div className="footer_text_dec">
+              <p>خرید زمین </p>
+              <p>خرید ویلا زیباکنار</p>
+              <p>ویلا دوبلکس </p>
+              <p>آپارتمان ارزان </p>
             </div>
-          
-            {/* تماس با ما */}
-            <div className="footer_texts_contactUs">
-              <p>تماس با ما</p>
-              <div>تلفن پشتیبانی : 09126248752 - 09126582211</div>
-              <div>آدرس : تهران - میدان مهر - شرکت گستران</div>
-              <div>ایمیل : amlak@gmail.com</div>
+          </div>
+
+          {/* تماس با ما */}
+          <div className="footer_texts_contactUs">
+            <p className="footer_text_title">تماس با ما</p>
+            <div className="footer_text_dec">
+              <p>تلفن پشتیبانی : 09126248752 - 09126582211</p>
+              <p>آدرس : تهران - میدان مهر - شرکت گستران</p>
+              <p>ایمیل : amlak@gmail.com</p>
               <div className="footer_texts_socials ">
-                <div>شبکه های اجتماعی :</div>
-                <div className="footer_texts_socials_icons col-5">
+                <p>شبکه های اجتماعی :</p>
+
+                <div className="footer_texts_socials_icons">
                   <div className="footer_texts_socials_icon_bg">
                     <Link href={"/"}>
                       <BiLogoInstagram className="footer_texts_socials_icon" />
@@ -84,26 +91,24 @@ export default function Footer() {
                 </div>
               </div>
             </div>
-            {/* درباره ما */}
-            <div className="footer_texts_aboutUs ">
-              <p>درباره ما</p>
-              <div className="footer_texts_aboutUs_desc">
-                به عنوان اولین سایت ملکی جهانی در ایران، بهترین مرجع برای ثبت آگهی
-                فروش و رهن و اجاره ملک می باشد.
-              </div>
-              <div className="footer_texts_email_title">
+          </div>
+          {/* درباره ما */}
+          <div className="footer_texts_aboutUs ">
+            <p className="footer_text_title">درباره ما</p>
+            <div className="footer_aboutUs_desc">
+              <p className="footer_texts_aboutUs_desc">
+                به عنوان اولین سایت ملکی جهانی در ایران، بهترین مرجع برای ثبت
+                آگهی فروش و رهن و اجاره ملک می باشد.
+              </p>
+              <p className="footer_email_title">
                 ایمیل خود را برای دریافت اخبار جدید وارد کنید.
-              </div>
-              <div className="footer_texts_email col-12">
-                <input
-                  type="email"
-                  className="col-9"
-                  placeholder=".ایمیل خود را وارد نمایید"
-                />
-                <button className="col-3">ثبت </button>
+              </p>
+              <div className="footer_texts_email ">
+                <input type="email" placeholder=".ایمیل خود را وارد نمایید" />
+                <button>ثبت </button>
               </div>
             </div>
-        
+          </div>
         </div>
         {/* bottom section */}
         <div className="footer_product ">
@@ -112,7 +117,7 @@ export default function Footer() {
             <div className="footer_product ">
               کلیه حقوق این سایت متعلق به شرکت کلان داده گستران می باشد.
             </div>
-            <div className="">
+            <div className="footer_product_img">
               <Image
                 src={"/image/footer/e-namad1.png"}
                 width={56}
